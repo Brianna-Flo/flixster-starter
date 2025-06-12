0394f6171a85308aa737e365ff0d1ceeafdee34e
Brianna Flores <briannaflores@meta.com>
2025-06-12 10:54:34 -0700

    this commit will add the favorite feature to movieCard

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -26,10 +26,9 @@ const Modal = ({ onCloseModal, modalData }) => {
           height="315"
           src={modalData.trailer}
           title="YouTube video player"
-          frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
         ></iframe>
         <button className="close" onClick={handleModalClose}>
           Close
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './MovieCard.css';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as heartOutline } from '@fortawesome/free-regular-svg-icons'
+import { useState, useEffect } from "react";
 
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
@@ -11,12 +15,24 @@ const MovieCard = ({data, onOpenModal, onLoadModal}) => {
         onLoadModal(data);
     }
 
+    const [favorite, setFavorite] = useState();
+    
+    const handleFavoriteClick = (event) => {
+        event.stopPropagation();
+        setFavorite((prev) => !prev)
+    }
+    const favorited = <FontAwesomeIcon icon={solidHeart} id="favorited" className="heart" onClick={handleFavoriteClick}/>
+    const notFavorited = <FontAwesomeIcon icon={heartOutline} id="not-favorited" className="heart" onClick={handleFavoriteClick}/>
+
     return (
         <div className="movie-card" onClick={() => handleCardClick()}>
             <img className="movie-poster" src={`${IMG_URL}${data.image}`} alt={data.title}/>
             <div className="movie-info">
                 <h4>{data.title}</h4>
                 <p>Rating: {data.rating}</p>
+            </div>
+            <div>
+                {favorite ? favorited : notFavorited}
             </div>
         </div>
     )
